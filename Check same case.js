521c8e17fa61063793613e745ab7dfc9aed0e0aa
AntonioKlenitsky <132097074+AntonioKlenitsky@users.noreply.github.com>
2023-12-19 08:57:35 +0000

    add new solution

--- Check same case.js
+++ Check same case.js
@@ -26,3 +26,13 @@ function sameCase(a, b) {
       return 0
     }
   }
+  //sol
+  function sameCase(a, b){
+    if(a.toUpperCase() === a.toLowerCase() || b.toLowerCase() === b.toUpperCase()){
+       return -1
+     }else if(a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+         return 1
+     }else{
+       return 0
+     }
+   }
