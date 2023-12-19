eb0772353f8d050c5e7d4be4749ced5c4186d08e
AntonioKlenitsky <132097074+AntonioKlenitsky@users.noreply.github.com>
2023-12-19 20:38:33 +0000

    solution

---  Enumerable Magic - Does My List Include This?.js
+++  Enumerable Magic - Does My List Include This?.js
@@ -1,2 +1,10 @@
 /*Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.*/
 
+//solution
+
+function include(arr, item){
+    if (arr.includes(item)){
+      return true;
+    }
+    return false;
+  }
